5595aca96502e20f0ac383b606e660b84a4ae1fa
Amani <799375868@qq.com>
2026-03-23 22:27:21 +0800

    UIController生成器

---
+++ Tools/Generata Tools/SourceGenerator/UIControllerGenerator/Definition.cs
@@ -0,0 +1,10 @@
+namespace UIControllerGenerator;
+
+public sealed class Definition
+{
+    public const string NameSpace = "GameLogic";
+    public static readonly string[] UsingNameSpace = ["System.Collections.Generic", "DGame"];
+    public static readonly string[] TargetNameSpaces = ["GameLogic"];
+    public const string InterfaceName = "IUIController";
+    public const string TargetClassName = "UIModule";
+}
